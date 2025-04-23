79e59ffea57562e942d92c27783b0caf68b01c59
AYOUB EL MIRI <103149836+ayoubmiri@users.noreply.github.com>
2025-04-23 22:52:05 +0100

    Update server.js

--- api-server/server.js
+++ api-server/server.js
@@ -5,7 +5,13 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://est-one.vercel.app/', // Remplacez par votre URL frontend
+    'http://localhost:3000'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(bodyParser.json());
 
 // Données mock pour les tâches
